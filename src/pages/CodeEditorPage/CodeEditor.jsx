24856ee4625c73534f394a953c1442e000453c40
MertJSX <mertJSX@proton.me>
2024-08-04 13:30:43 +0300

    Modifed CodeEditor.jsx

--- src/pages/CodeEditorPage/CodeEditor.jsx
+++ src/pages/CodeEditorPage/CodeEditor.jsx
@@ -3,7 +3,6 @@ import CodeEditor from '../../components/CodeEditor/CodeEditor';
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import moment from 'moment';
 import { useNavigate, useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 
@@ -14,66 +13,33 @@ const CodeEditorPage = () => {
     const [fileContent, setFileContent] = useState("")
     const path = params.path;
     const [res, setRes] = useState("")
-    const [lastModified, setLastModified] = useState("")
     const [readOnly, setReadOnly] = useState(false);
     const [fileTitle, setFileTitle] = useState("")
-    const [autoupdate, setAutoupdate] = useState(true);
     const socket = useRef();
 
     function handleEditorChange(value, event) {
-        // event.preventDefault();
         console.log("Editor change");
         console.log(event);
-        let fileContentMem = fileContent;
 
-        // setReadOnly(!readOnly);
-        // setReadOnly(!readOnly);
-        // setFileContent("");
-        // setFileContent(fileContentMem)
-        
-        
-        // setTimeout(() => {
-        //     setFileContent(fileContent);
-        // }, 1000);
-        
-        socket.current.emit("change-file", {path: path.slice(1), content: value})
-    }
-    function saveFile() {
-        console.log(fileContent);
-        axios.post(`${Cookies.get("ip")}/api/write-file?password=${Cookies.get("password")}&path=${path.slice(1)}&type=change`, {
-            content: fileContent
-        }).then((data) => {
-            console.log("Last modified is setting to ", data.data);
-            console.log(moment(data.data.lastModified).format("Do MMMM YYYY HH:mm:ss"));
-            setLastModified(data.data.lastModified);
-            setRes(data.data.response)
-            setTimeout(() => {
-                setRes("");
-            }, 1500);
-        })
+        socket.current.emit("change-file", { path: path.slice(1), content: value })
     }
     function readFile(isUpdate = false) {
-        axios.get(`${Cookies.get("ip")}/api/read-file?password=${Cookies.get("password")}&filepath=${path.slice(1)}`).then((data) => {
+        axios.post(`${Cookies.get("ip")}/api/read-file?filepath=${path.slice(1)}`, 
+        { username: Cookies.get("username"), password: Cookies.get("password") }).then((data) => {
             console.log(data.data);
             if (data.data.res) {
                 if (isUpdate) {
                     // setFileContent(data.data.data);
-                    console.log("Only sex");
-                    
                     console.log(data.data);
-                    socket.current.emit("change-file", {path: path.slice(1), content: data.data.data})
-                    setLastModified(data.data.lastModified)
+                    socket.current.emit("change-file", { path: path.slice(1), content: data.data.data })
                     setRes("Updated!")
                     setReadOnly(!data.data.writePermission)
                     setTimeout(() => {
                         setRes("")
                     }, 1000);
                 } else {
-                    console.log("Sexy");
-                    
                     setFileTitle(data.data.title);
                     setFileContent(data.data.data);
-                    setLastModified(data.data.lastModified);
                     setReadOnly(!data.data.writePermission);
                     setEditorLanguage(detectFileLanguage());
                 }
@@ -112,35 +78,12 @@ const CodeEditorPage = () => {
             navigate("/login");
         }
     }, [])
-    // useEffect(() => {
-    //     if (socket.current && fileContent !== undefined) {
-    //         console.log("Change file emit");
-    //         console.log("file content is",fileContent);
-            
-            
-    //         socket.current.emit("change-file", {path: path.slice(1), content: fileContent})
-    //     }
-    // }, [fileContent])
+
     useEffect(() => {
-        if (socket.current) {
-            console.log("Socket was updated now");
-            socket.current.on('file-changed', (res) => {
-                console.log(res);
-                console.log("File changed");
-                console.log(socket.current.id);
-                
-                // let first = moment(lastModified).format("Do MMMM YYYY HH:mm:ss")
-                // let second = moment(res.lastModified).format("Do MMMM YYYY HH:mm:ss")
-                // console.log(`${first} !== ${second}`);
-                if (res.path === path.slice(1)) {
-                    setFileContent(res.fileContent)
-                }
-            });
-        }
         if (!socket.current) {
             socket.current = io(Cookies.get("ip"), { auth: { password: Cookies.get("password"), watch: path.slice(1) } });
             socket.current.on('connect_error', (err) => {
-                console.log("Socket connect error");
+                console.error("Socket connect error");
                 setTimeout(() => {
                     setRes(`Socket: ${err.message}`)
                     setTimeout(() => {
@@ -149,44 +92,25 @@ const CodeEditorPage = () => {
                 }, 3000);
             })
             socket.current.on('connect', () => {
-                let currentModified = lastModified;
                 console.log('Connected to the server');
-
                 socket.current.on('file-changed', (res) => {
-                    console.log(res);
-                    console.log("File changed");
-                    console.log(socket.current.id);
-                    
-                    let first = moment(currentModified).format("Do MMMM YYYY HH:mm:ss")
-                    let second = moment(res.lastModified).format("Do MMMM YYYY HH:mm:ss")
-                    console.log(`${first} !== ${second}`);
-                    if (res.path === path.slice(1) && res.lastModified !== currentModified) {
-                        if (autoupdate) {
-                            console.log(res);
-                            setFileContent(res.fileContent)
-                            // readFile(true);
-                        } else {
-                            setRes("Outdated client!");
-                            console.log(res);
-                        }
+                    if (res.path === path.slice(1)) {
+                        setFileContent(res.fileContent)
                     }
                 });
-
                 socket.current.on('error', (res) => {
-                    console.log(res);
+                    console.error(res);
                     setRes(res.err)
                     setTimeout(() => {
                         setRes("")
                     }, 5000);
                 });
-
                 socket.current.on('disconnect', (reason) => {
                     console.log(`Disconnected from the server: ${reason}`);
                     socket.current = null;
                 });
             });
         }
-
 
         return () => {
             if (socket.current) {
@@ -197,7 +121,7 @@ const CodeEditorPage = () => {
                 socket.current.off('connect_error');
             }
         };
-    }, [lastModified])
+    }, [])
 
     return (
         <div>
